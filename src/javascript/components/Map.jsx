a9eff12273484505b3705add8b4fda740e157beb
Justina <Justez@users.noreply.github.com>
2018-10-28 18:45:18 +0200

    Add map with cute icon in a bubble

--- src/javascript/components/Map.jsx
+++ src/javascript/components/Map.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import GoogleMapReact from 'google-map-react';
 import '../../assets/stylesheets/components/Map.sass';
 import PropTypes from 'prop-types';
 
-const Map = ({ coordinates }) => {
+const WeatherInfo = ({ icon, weatherIcons }) => (
+  <div className="Map-info">
+    <div className="Map-info-details">
+      <img
+        alt={icon}
+        src={weatherIcons[icon]}
+      />
+    </div>
+  </div>
+);
+
+const Map = ({ coordinates, weather, weatherIcons }) => {
   if (coordinates.lat) {
     return (
       <div className="Map-wrapper background">
         <div className="Map">
-          {coordinates.lat && JSON.stringify(coordinates)}
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: 'AIzaSyAa2OXPttUPWWRvUJsSv4WqZHLE-h_fgjo' }}
+            defaultCenter={coordinates}
+            defaultZoom={11}
+          >
+            <WeatherInfo
+              {...coordinates}
+              icon={weather && weather.weather ? weather.weather[0].icon : ''}
+              weatherIcons={weatherIcons}
+            />
+          </GoogleMapReact>
         </div>
       </div>
     );
@@ -26,8 +48,20 @@ Map.defaultProps = {
   coordinates: {},
 };
 
+WeatherInfo.propTypes = {
+  icon: PropTypes.string,
+  weatherIcons: PropTypes.objectOf(PropTypes.string),
+};
+
+WeatherInfo.defaultProps = {
+  icon: '',
+  weatherIcons: {},
+};
+
 const mapStateToProps = state => ({
   coordinates: state.app.coordinates,
+  weather: state.app.currentWeather,
+  weatherIcons: state.app.weatherIcons,
 });
 
 export default connect(mapStateToProps, undefined)(Map);
--- src/javascript/redux/reducers/mainReducer.js
+++ src/javascript/redux/reducers/mainReducer.js
@@ -35,7 +35,6 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
-  console.log(action);
   switch (action.type) {
     case 'SET_LOCATION_COORDS':
       return {
